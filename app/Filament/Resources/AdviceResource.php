9d589e775231bd319898aa74dc905384f44cb134
Asim Thapa <athapa@lumination.com.au>
2023-03-08 18:34:13 +1030

    WIP user management

---
+++ app/Filament/Resources/AdviceResource.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AdviceResource\Pages;
+use App\Filament\Resources\AdviceResource\RelationManagers;
+use App\Models\Advice;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+
+
+class AdviceResource extends Resource
+{
+    protected static ?string $model = Advice::class;
+
+    protected static ?string $navigationGroup = 'Content Management';
+    protected static ?string $navigationGroupIcon = 'heroicon-o-collection';
+
+    protected static ?int $navigationSort = 2;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    // protected static bool $shouldRegisterNavigation = false;
+
+    public static function form(Form $form): Form
+    {
+        $user = Auth::user()->full_name;
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        Forms\Components\TextInput::make('post_title')
+                            ->label('Title')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\RichEditor::make('post_content')
+                            ->label('Content')
+                            ->required()
+                            ->maxLength(65535),
+                        Forms\Components\RichEditor::make('post_excerpt')
+                            ->required()
+                            ->label('Excerpt')
+                            ->disableToolbarButtons([
+                                'attachFiles',
+                            ]),
+                        Forms\Components\Grid::make(2)
+                            ->schema([
+                                Forms\Components\TextInput::make('Author')
+                                    ->default($user)
+                                    ->disabled(),
+                                Forms\Components\Select::make('post_status')
+                                    ->options([
+                                        'Published' => 'Published',
+                                        'Unpublished' => 'Unpublished',
+                                        'Draft' => 'Draft',
+                                        'Pending' => 'Pending'
+                                    ])
+                                    ->label('Status')
+                                    ->required(),
+                            ])
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('post_title')
+                    ->label('Title')
+                    ->sortable()
+                    ->searchable(),
+                // Tables\Columns\TextColumn::make('post_type')
+                //     ->label('Type')
+                //     ->sortable()
+                //     ->searchable(),
+                Tables\Columns\TextColumn::make('author.full_name')->label('Author'),
+                Tables\Columns\TextColumn::make('post_date')
+                    ->date()
+                    ->label('Created At'),
+                Tables\Columns\TextColumn::make('post_modified')
+                    ->date()
+                    ->label('Modified At'),
+                Tables\Columns\TextColumn::make('post_status')
+                    ->label('Status')
+                    ->sortable()
+                    ->searchable(),
+
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAdvice::route('/'),
+            'create' => Pages\CreateAdvice::route('/create'),
+            'edit' => Pages\EditAdvice::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/SiteResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SiteResource\Pages;
+use App\Filament\Resources\SiteResource\RelationManagers;
+use App\Models\Site;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SiteResource extends Resource
+{
+    protected static ?string $model = Site::class;
+
+    protected static ?string $navigationGroup = 'User Management';
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Card::make()
+                    ->schema([
+                        // Forms\Components\TextInput::make('site_uid')
+                        //     ->maxLength(255),
+                        Forms\Components\TextInput::make('site_name')
+                            ->required()
+                            ->label('Site Name')
+                            ->maxLength(255),
+                        Forms\Components\Textarea::make('site_value')
+                            ->required()
+                            ->label('Site Value')
+                            ->maxLength(65535),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('site_uid')->label('uid'),
+                Tables\Columns\TextColumn::make('site_name')->label('name'),
+                Tables\Columns\TextColumn::make('site_value')->label('value')->wrap(),
+                // Tables\Columns\TextColumn::make('created_at')
+                //     ->dateTime(),
+                // Tables\Columns\TextColumn::make('updated_at')
+                //     ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                // Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSites::route('/'),
+            'create' => Pages\CreateSite::route('/create'),
+            'edit' => Pages\EditSite::route('/{record}/edit'),
+        ];
+    }
+}
