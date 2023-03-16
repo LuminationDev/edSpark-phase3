9bd47461a96a9c93a5553d5423cc45d1d1423022
Erick Hartawan <ehartawan@lumination.com.au>
2023-03-16 11:12:49 +1030

    Added EditorJs with some basic plugins. Started structuring SchoolSingle page with components and created a SchoolTechIconGenerator

---
+++ resources/js/components/schoolsingle/SchoolTech.vue
@@ -0,0 +1,34 @@
+<script setup>
+import LaptopGear from "@/js/components/svg/LaptopGear.vue";
+import SchoolTechIconGenerator from "@/js/components/global/SchoolTechIconGenerator.vue";
+
+const props = defineProps({
+    techList:{
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <div class="bg-white border-[0.5px] border-black px-5 py-8 flex flex-col relative overflow-hidden max-w-xs">
+        <div class="absolute techListBackground -right-28 -top-24 ">
+            <LaptopGear />
+        </div>
+        <div class="techListTitle font-semibold text-lg mb-5">
+            Tech used at this school
+        </div>
+        <div
+            v-for="(tech,index) in techList"
+            :key="index"
+            class="w-full flex pb-4 basis-1/4"
+        >
+            <SchoolTechIconGenerator
+                :tech-name="tech.name"
+                class="min-w-[60px] pr-4 m-2"
+            />
+            <p class="text-base font-light basis-3/4 ">
+                {{ tech.description }}
+            </p>
+        </div>
+    </div>
+</template>
