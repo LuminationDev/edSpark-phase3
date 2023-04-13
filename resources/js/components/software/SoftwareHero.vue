957dc58b6dca8c2a29ca231f648217b65ef00f85
Erick Hartawan <ehartawan@lumination.com.au>
2023-04-13 11:24:25 +0930

    WIP init Softwarew page

---
+++ resources/js/components/software/SoftwareHero.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import BaseHero from "@/js/components/hero/BaseHero.vue";
+import SoftwareRobot from "@/js/components/svg/software/SoftwareRobot.vue";
+</script>
+<template>
+    <BaseHero
+        color1="#002858"
+        color2="#4F2D8D"
+        color3="#C7B2EA"
+        class="!h-fit mb-16"
+    >
+        <template #titleText>
+            {{ `Software` }}
+        </template>
+        <template #subtitleText1>
+            {{ `Find out what software is available to you as an employee and how to access them` }}
+        </template>
+        <template #icon>
+            <SoftwareRobot />
+        </template>
+    </BaseHero>
+</template>
