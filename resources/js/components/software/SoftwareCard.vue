28d26841e7f7d7e76fa46df93e437dd2fb5f279c
Erick Hartawan <ehartawan@lumination.com.au>
2023-04-27 12:15:56 +0930

    further work with like and bookmark. more stable and data kept in store

--- resources/js/components/software/SoftwareCard.vue
+++ resources/js/components/software/SoftwareCard.vue
@@ -2,27 +2,42 @@
 import GenericCard from "@/js/components/card/GenericCard.vue";
 import SoftwareCardIcon from "@/js/components/software/SoftwareCardIcon.vue";
 import {useRouter} from "vue-router";
+import {likeURL, bookmarkURL} from "@/js/constants/serverUrl";
+
 const props = defineProps({
     software: {
         type: Object,
         required: true
     },
-    numberPerRow:{
+    numberPerRow: {
         type: Number,
         required: false,
         default: 3
+    },
+    key:{
+        type: Number,
+        required: true
     }
 })
-console.log(props)
 const router = useRouter()
+
 const handleClickCard = () => {
     /**
      * id inside param must be the same as the route path specified for advice-single
+     * which is /software/resources/:id
      */
-    // which is /advice/resources/:id
-    router.push({ name:"software-single", params: {id: props.software.post_id, content: JSON.stringify(props.software) }})
-
+    router.push({
+        name: "software-single",
+        params: {id: props.software.post_id, content: JSON.stringify(props.software)}
+    })
 }
+
+const likeBookmarkData = {
+    post_id: props.software.post_id,
+    user_id: 2, // to be replaced with userId from userStore
+    post_type: 'software'
+}
+
 </script>
 <template>
     <GenericCard
@@ -32,6 +47,7 @@ const handleClickCard = () => {
         :cover-image="software['cover_image']"
         :number-per-row="props.numberPerRow"
         :click-callback="handleClickCard"
+        :like-bookmark-data="likeBookmarkData"
     >
         <template #icon>
             <SoftwareCardIcon
