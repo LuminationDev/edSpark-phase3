a3bd526058004e83d5676ef3908e52668d04be8e
Erick Hartawan <ehartawan@lumination.com.au>
2023-04-27 12:15:56 +0930

    further work with like and bookmark. more stable and data kept in store

---
+++ resources/js/components/global/Tooltip/Tooltip.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+    tip: {
+        type: String,
+        required: true
+    },
+
+    toolTipMargin: {
+        type: String,
+        default: '-ml-1.5'
+    }
+})
+</script>
+
+<template>
+    <span
+        :class="toolTipMargin"
+        class="tooltip rounded shadow-lg px-3 py-1 bg-gray-700 rounded-xl text-white -mt-8"
+    >{{ tip }}</span>
+</template>
+
+<style scoped>
+.tooltip {
+    @apply invisible absolute;
+}
+
+.has-tooltip:hover .tooltip {
+    visibility: visible;
+    animation: appear 1s;
+}
+
+@keyframes appear {
+    0% {
+        opacity: 0
+    }
+
+    100% {
+        opacity: 1;
+        z-index: 50;
+    }
+}
+</style>
--- resources/js/components/software/SoftwareCardGallery.vue
+++ resources/js/components/software/SoftwareCardGallery.vue
@@ -1,12 +1,51 @@
 <script setup>
 import axios from 'axios'
-import {onBeforeMount, ref} from "vue";
 import {serverURL} from "@/js/constants/serverUrl";
 import SoftwareCard from "@/js/components/software/SoftwareCard.vue";
 import {axiosFetcher} from "@/js/helpers/fetcher";
 import useSWRV from "swrv";
+import {useUserStore} from "@/js/stores/useUserStore";
+import {storeToRefs} from "pinia";
 
 const {data: softwareList , error} = useSWRV(`${serverURL}/fetchSoftwarePosts`, axiosFetcher)
+
+/**
+ * not needed once the fetch is done on the dashbaord. maybe have a seperate update or check here for each type
+ * @type {{user_id: number, post_type: string}}
+ */
+const query = {
+    user_id: 2,
+    post_type: 'software'
+}
+
+const userStore = useUserStore()
+const {userLikeList, userBookmarkList} = storeToRefs(userStore)
+
+axios.post(`${serverURL}/fetchAllLikesByType`, query).then(res => {
+    let temp = []
+    // get id Number only
+    for(let x of res.data){
+        temp.push(x.post_id)
+    }
+    userLikeList.value.software = temp
+
+}).catch(err => {
+    console.log(`there has been an error while fetching users like`)
+    console.log(err.message)
+})
+
+axios.post(`${serverURL}/fetchAllBookmarksByType`, query).then(res => {
+    let temp = []
+    // get id Number only
+    for(let x of res.data){
+        temp.push(x.post_id)
+    }
+    userBookmarkList.value.software = temp
+
+}).catch(err => {
+    console.log(`there has been an error while fetching users like`)
+    console.log(err.message)
+})
 
 </script>
 <template>
