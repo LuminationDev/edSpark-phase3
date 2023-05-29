dd9795a6d32cbe4d336208564089d243da077119
Jake Mackinlay <jmackinlay@lumination.com.au>
2023-05-29 14:32:51 +0930

    separated advice type fetcher and used useSWRVState function to handle loading states

--- app/Http/Controllers/AdviceController.php
+++ app/Http/Controllers/AdviceController.php
@@ -64,55 +64,34 @@ class AdviceController extends Controller
     }
 
     public function fetchAdvicePostByType($type) {
-        $adviceTypes = Advicetype::where('advice_type_name', $type)->first();
-        $adviceArticles = Advice::where('advicetype_id', $adviceTypes->id)->get();
+        $typeArray = explode(',', $type);
+        $data = [];
 
-        dd($adviceTypes->id);
+        foreach($typeArray as $typeItem) {
+            $adviceTypes = Advicetype::where('advice_type_name', $typeItem)->first();
+            $adviceArticles = Advice::where('advicetype_id', $adviceTypes->id)->get();
 
-        foreach($adviceArticles as $advice) {
-            dd($advice);
+            foreach ($adviceArticles as $advice) {
+                $result = [
+                    'post_id' => $advice->id,
+                    'post_title' => $advice->post_title,
+                    'post_content' => $advice->post_content,
+                    'post_excerpt' => $advice->post_excerpt,
+                    'author' => $advice->author->full_name,
+                    'cover_image' => ($advice->cover_image) ? $advice->cover_image : NULL,
+                    'template' => ($advice->template) ? $advice->template : NULL,
+                    'extra_content' => ($advice->extra_content) ? $advice->extra_content : NULL,
+                    'post_date' => $advice->post_date,
+                    'post_modified' => $advice->post_modified,
+                    'post_status' => $advice->post_status,
+                    'advice_type' => ($advice->advicetypes) ? $advice->advicetypes->pluck('advice_type_name') : NULL ,
+                    'created_at' => $advice->created_at,
+                    'updated_at' => $advice->updated_at
+                ];
+                $data[] = $result;
+            }
         }
-
-
-
-
-        // $adviceTypes = Advicetype::where('advice_type_name', $type)->get();
-        // $adviceArticles = Advice::where('advicetype_id', $adviceTypes->id)->get();
-
-
-
-
-
-        // $advices = Advice::where('post_status', 'Published')->orderBy('created_at', 'DESC')->get();
-        // $error = '';
-        // $data = [];
-
-        // forEach( $advices as $advice ) {
-        //     if ($advice->advicetypes->pluck('advice_type_name' ===  $type)) {
-        //         $res = [
-        //             'post_id' => $advice->id,
-        //             'post_title' => $advice->post_title,
-        //             'post_content' => $advice->post_content,
-        //             'post_excerpt' => $advice->post_excerpt,
-        //             'author' => $advice->author->full_name,
-        //             'cover_image' => ($advice->cover_image) ? $advice->cover_image : NULL,
-        //             'template' => ($advice->template) ? $advice->template : NULL,
-        //             'extra_content' => ($advice->extra_content) ? $advice->extra_content : NULL,
-        //             'post_date' => $advice->post_date,
-        //             'post_modified' => $advice->post_modified,
-        //             'post_status' => $advice->post_status,
-        //             'advice_type' => ($advice->advicetypes) ? $advice->advicetypes->pluck('advice_type_name') : NULL ,
-        //             'created_at' => $advice->created_at,
-        //             'updated_at' => $advice->updated_at
-        //         ];
-
-        //         $data[] = $res;
-
-        //     }
-        // }
-
-        // return response()->json($data);
-
+        return response()->json($data);
     }
 }
 
