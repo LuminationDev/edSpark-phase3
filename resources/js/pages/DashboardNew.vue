41e6a4d14d9b9ed1633e6942749d944d2e2563e3
Erick Hartawan <ehartawan@lumination.com.au>
2023-06-08 15:00:58 +0930

    added fallback userid to 9999

--- resources/js/pages/DashboardNew.vue
+++ resources/js/pages/DashboardNew.vue
@@ -86,6 +86,7 @@ const getIdToken = async () => {
         })
     } catch(e){
         console.warn('Failed to get Auth data. User is not logged in')
+        currentUser.value.id = 99999 // fallback user id
     }
 };
 
