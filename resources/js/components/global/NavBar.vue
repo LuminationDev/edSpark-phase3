d31f726cbe7c9e20ee627cbb1582b407f05ba752
Erick Hartawan <ehartawan@lumination.com.au>
2023-06-21 10:45:20 +0930

    casted engorgio to tech desc inside map

--- resources/js/components/global/NavBar.vue
+++ resources/js/components/global/NavBar.vue
@@ -92,7 +92,7 @@ setupRoutes();
         />
 
         <Logo
-            class="absolute right-20 top-36 z-30 md:w-44 md:h-44 md:top-24 sm:w-36 sm:h-36 sm:top-32 w-36 h-36 lg:top-24"
+            class="absolute right-20 top-16 z-30 md:w-44 md:h-44 md:top-10 sm:w-36 sm:h-36 sm:top-32 w-36 h-36 lg:top-24"
         />
         <NavSwoosh class="w-full absolute -bottom-6 left-0 right-0 pointer-events-none" />
     </div>
