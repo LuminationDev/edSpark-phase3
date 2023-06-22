74e655f7d771b21fa67b13be096760df9f2958d5
jlundlumination <jmackinlay@lumination.com.au>
2023-06-22 23:13:54 +0000

    small changes from the cloud. htpasswd and title icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,9 +13,7 @@
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
     {{-- <link rel="stylesheet" href="{{ mix('resources/css/app.css')}}" type="text/css"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}" type="text/css"> --}}
-
-
-{{--    <link rel="icon" type="image/x-icon" href="{{ mix('resources/assets/svg/Logo.svg') }}">--}}
+    <link rel="icon" type="image/x-icon" href="{{ asset('build/assets/Logo.svg') }}">
     @vite(['resources/js/app.js'])
 
 </head>
