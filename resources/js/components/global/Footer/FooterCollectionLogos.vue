8630684fb089a677ea9b20294596be422661618d
Erick Hartawan <ehartawan@lumination.com.au>
2023-08-04 11:09:57 +0930

    added links footer elements, currently pointing at DFE

--- resources/js/components/global/Footer/FooterCollectionLogos.vue
+++ resources/js/components/global/Footer/FooterCollectionLogos.vue
@@ -5,39 +5,54 @@
 <template>
     <div class="FooterImagesRow flex justify-between items-center flex-col w-full md:!flex-row">
         <div class="h-20 p-4 w-40">
-            <img
-                src="../../../../assets/images/footer/cc.png"
-                alt=""
-                class="h-full object-contain w-full"
-            >
+            <a href="https://creativecommons.org/licenses/by/4.0/">
+                <img
+                    src="../../../../assets/images/footer/cc.png"
+                    alt=""
+                    class="h-full object-contain w-full"
+                >
+
+            </a>
         </div>
         <div class="h-20 p-4 w-40">
-            <img
-                src="../../../../assets/images/footer/south-australia.png"
-                alt=""
-                class="h-full object-contain w-full"
-            >
+            <a href="https://southaustralia.com/">
+                <img
+                    src="../../../../assets/images/footer/south-australia.png"
+                    alt=""
+                    class="h-full object-contain w-full"
+                >
+
+            </a>
         </div>
         <div class="h-20 p-4 w-40">
-            <img
-                src="../../../../assets/images/footer/sagovau_rev.png"
-                alt=""
-                class="h-full object-contain w-full"
-            >
+            <a href="https://www.sa.gov.au/">
+                <img
+                    src="../../../../assets/images/footer/sagovau_rev.png"
+                    alt=""
+                    class="h-full object-contain w-full"
+                >
+
+            </a>
         </div>
         <div class="h-20 p-4 w-40">
-            <img
-                src="../../../../assets/images/footer/white-ribbon.svg"
-                alt=""
-                class="h-full object-contain w-full"
-            >
+            <a href="https://www.whiteribbon.org.au/">
+                <img
+                    src="../../../../assets/images/footer/white-ribbon.svg"
+                    alt=""
+                    class="h-full object-contain w-full"
+                >
+
+            </a>
         </div>
         <div class="h-20 p-4 w-40">
-            <img
-                src="../../../../assets/images/footer/dfe.svg"
-                alt=""
-                class="h-full object-contain w-full"
-            >
+            <a href="https://www.education.sa.gov.au/">
+                <img
+                    src="../../../../assets/images/footer/dfe.svg"
+                    alt=""
+                    class="h-full object-contain w-full"
+                >
+
+            </a>
         </div>
     </div>
 </template>
