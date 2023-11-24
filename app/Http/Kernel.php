ce437b1a4a04e6361ec5db819eb56c0f30c02433
Erick Hartawan <ehartawan@lumination.com.au>
2023-11-24 14:38:25 +1030

    WIP FilterPartnerResponses

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\FilterPartnerResponse;
 use App\Http\Middleware\ResourceAccessControl;
 use Filament\Resources\Resource;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
@@ -46,7 +47,8 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            ResourceAccessControl::class
+            ResourceAccessControl::class,
+            FilterPartnerResponse::class
         ],
     ];
 
