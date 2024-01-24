93679c8f441e5431d38add0da2c9fc2d79f159f8
Erick Hartawan <ehartawan@lumination.com.au>
2024-01-25 10:07:34 +1030

    much improved import tool from filament

--- app/Filament/Resources/CatalogueResource/Pages/ListCatalogues.php
+++ app/Filament/Resources/CatalogueResource/Pages/ListCatalogues.php
@@ -2,11 +2,15 @@
 
 namespace App\Filament\Resources\CatalogueResource\Pages;
 
+use App\Filament\Imports\CatalogueImporter;
 use App\Filament\Resources\CatalogueResource;
+use App\Models\Catalogue;
 use Filament\Actions;
+use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
-use Konnco\FilamentImport\Actions\ImportAction;
-use Konnco\FilamentImport\Actions\ImportField;
+
+//use Konnco\FilamentImport\Actions\ImportAction;
+//use Konnco\FilamentImport\Actions\ImportField;
 
 class ListCatalogues extends ListRecords
 {
@@ -15,66 +19,67 @@ class ListCatalogues extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
             ImportAction::make()
-                ->fields([
-                    ImportField::make('unique_reference')
-                        ->label('Unique Reference'),
-                    ImportField::make('type')
-                        ->label('Type'),
-                    ImportField::make('brand')
-                        ->label('Brand'),
-                    ImportField::make('name')
-                        ->label('Name'),
-                    ImportField::make('vendor')
-                        ->label('Vendor'),
-                    ImportField::make('category')
-                        ->label('Category'),
-                    ImportField::make('price_inc_gst')
-                        ->label('Price Including GST'),
-                    ImportField::make('processor')
-                        ->label('Processor'),
-                    ImportField::make('storage')
-                        ->label('Storage'),
-                    ImportField::make('memory')
-                        ->label('Memory'),
-                    ImportField::make('form_factor')
-                        ->label('Form Factor'),
-                    ImportField::make('display')
-                        ->label('Display'),
-                    ImportField::make('graphics')
-                        ->label('Graphics'),
-                    ImportField::make('wireless')
-                        ->label('Wireless'),
-                    ImportField::make('webcam')
-                        ->label('Webcam'),
-                    ImportField::make('operating_system')
-                        ->label('Operating System'),
-                    ImportField::make('warranty')
-                        ->label('Warranty'),
-                    ImportField::make('battery_life')
-                        ->label('Battery Life'),
-                    ImportField::make('weight')
-                        ->label('Weight'),
-                    ImportField::make('stylus')
-                        ->label('Stylus'),
-                    ImportField::make('other')
-                        ->label('Other'),
-                    ImportField::make('available_now')
-                        ->label('Available Now'),
-                    ImportField::make('corporate')
-                        ->label('Corporate'),
-                    ImportField::make('administration')
-                        ->label('Administration'),
-                    ImportField::make('curriculum')
-                        ->label('Curriculum'),
-                    ImportField::make('image')
-                        ->label('Image'),
-                    ImportField::make('product_number')
-                        ->label('Product Number'),
-                    ImportField::make('price_expiry')
-                        ->label('Price Expiry'),
-                ]),
+            ->importer(CatalogueImporter::class)
+//            ImportAction::make()
+//                ->fields([
+//                    ImportField::make('unique_reference')
+//                        ->label('Unique Reference'),
+//                    ImportField::make('type')
+//                        ->label('Type'),
+//                    ImportField::make('brand')
+//                        ->label('Brand'),
+//                    ImportField::make('name')
+//                        ->label('Name'),
+//                    ImportField::make('vendor')
+//                        ->label('Vendor'),
+//                    ImportField::make('category')
+//                        ->label('Category'),
+//                    ImportField::make('price_inc_gst')
+//                        ->label('Price Including GST'),
+//                    ImportField::make('processor')
+//                        ->label('Processor'),
+//                    ImportField::make('storage')
+//                        ->label('Storage'),
+//                    ImportField::make('memory')
+//                        ->label('Memory'),
+//                    ImportField::make('form_factor')
+//                        ->label('Form Factor'),
+//                    ImportField::make('display')
+//                        ->label('Display'),
+//                    ImportField::make('graphics')
+//                        ->label('Graphics'),
+//                    ImportField::make('wireless')
+//                        ->label('Wireless'),
+//                    ImportField::make('webcam')
+//                        ->label('Webcam'),
+//                    ImportField::make('operating_system')
+//                        ->label('Operating System'),
+//                    ImportField::make('warranty')
+//                        ->label('Warranty'),
+//                    ImportField::make('battery_life')
+//                        ->label('Battery Life'),
+//                    ImportField::make('weight')
+//                        ->label('Weight'),
+//                    ImportField::make('stylus')
+//                        ->label('Stylus'),
+//                    ImportField::make('other')
+//                        ->label('Other'),
+//                    ImportField::make('available_now')
+//                        ->label('Available Now'),
+//                    ImportField::make('corporate')
+//                        ->label('Corporate'),
+//                    ImportField::make('administration')
+//                        ->label('Administration'),
+//                    ImportField::make('curriculum')
+//                        ->label('Curriculum'),
+//                    ImportField::make('image')
+//                        ->label('Image'),
+//                    ImportField::make('product_number')
+//                        ->label('Product Number'),
+//                    ImportField::make('price_expiry')
+//                        ->label('Price Expiry'),
+//                ]),
 
         ];
     }
