1f169fa8375b59a2689b30fc2ae3db0eebc1b7fc
Erick Hartawan <ehartawan@lumination.com.au>
2024-01-25 15:02:05 +1030

    small tweaks on catalogue table display

--- app/Filament/Resources/CatalogueResource/Pages/ListCatalogues.php
+++ app/Filament/Resources/CatalogueResource/Pages/ListCatalogues.php
@@ -2,18 +2,13 @@
 
 namespace App\Filament\Resources\CatalogueResource\Pages;
 
-use App\Filament\Exports\CatalogueExporter;
 use App\Filament\Imports\CatalogueImporter;
 use App\Filament\Resources\CatalogueResource;
 use App\Models\Catalogue;
 use Filament\Actions\Action;
-use Filament\Actions\ExportAction;
-use Filament\Actions\Exports\Enums\ExportFormat;
-use Filament\Actions\Exports\Models\Export;
 use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
+use Filament\Support\Colors\Color;
 
 
 class ListCatalogues extends ListRecords
@@ -28,7 +23,7 @@ class ListCatalogues extends ListRecords
 //                ->fileName(fn (Export $export): string => "catalogue-" . now()->format('dmY') . ".csv"),
             Action::make('Delete all')
                 ->icon('heroicon-m-x-mark')
-                ->color('danger')
+                ->color(Color::Rose)
                 ->requiresConfirmation()
                 ->action(function () {
                     Catalogue::deleteAll();
