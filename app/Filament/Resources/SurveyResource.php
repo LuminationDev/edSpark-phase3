c6ab93cd7dcdf4a44da4e09e3dad259061cf3948
Jan <andrew.martin@codefishstudio.com>
2024-02-07 14:04:00 +1030

    DMA survey table, admin page for DMA survey creation

---
+++ app/Filament/Resources/SurveyResource.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FeedbackResource\Pages;
+use App\Filament\Resources\FeedbackResource\RelationManagers;
+use App\Helpers\RoleHelpers;
+use App\Models\Survey;
+use Closure;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Support\Facades\Log;
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
+
+class SurveyResource extends Resource
+{
+    protected static ?string $model = Survey::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-academic-cap';
+    protected static ?string $navigationGroup = 'DMA Tool';
+    protected static ?string $modelLabel = 'Version';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Section::make()->schema([
+                    Forms\Components\FileUpload::make('json_file')
+                        ->acceptedFileTypes(['application/json'])
+                        ->storeFiles(false)
+                        ->rules([
+                            fn(): Closure => function (string $attribute, $value, Closure $fail) {
+                                $errmsg = self::validateFile($value);
+                                if ($errmsg !== null) {
+                                    $fail($errmsg);
+                                }
+                            },
+                        ])
+                        ->required(),
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('version')
+                    ->label('Version')
+                    ->limit(25)
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\ToggleColumn::make('is_active')
+                    ->label('Is Active')
+                    ->disabled(fn($record) => $record->is_active)
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->sortable()
+                    ->dateTime()
+                    ->label('Created at'),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->sortable()
+                    ->dateTime()
+                    ->label('Last Updated'),
+            ])
+            ->defaultSort('version', 'desc');
+//            ->actions([
+//                Tables\Actions\EditAction::make(),
+//            ])
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSurveys::route('/'),
+            'create' => Pages\NewSurvey::route('/create'),
+        ];
+    }
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return RoleHelpers::has_minimum_privilege('moderator');
+    }
+
+    protected static array $domains = ['triage', 'teaching', 'managing', 'leading', 'learning'];
+
+
+    /**
+     *
+     * Validate that an inputted JSON file is a valid survey.
+     * @param TemporaryUploadedFile $file
+     * @return string|null: error message or null for valid file
+     */
+    private static function validateFile(TemporaryUploadedFile $file): ?string
+    {
+        try {
+            $contents = $file->get();
+            if ($contents === null) {
+                // invalid file
+                return 'File is invalid';
+            }
+            if(!json_validate($contents)) {
+                // invalid json
+                return 'JSON is invalid';
+            }
+
+            $json = json_decode($contents, true);
+            if ($json === null) {
+                // no json in file
+                return 'No JSON present';
+            }
+
+            if (!array_key_exists('questions', $json) || !is_array($json['questions'])) {
+                // expected an array
+                return 'Expected an object containing a \'questions\' array ';
+            }
+            if(count($json['questions']) === 0) {
+                return 'Survey is empty - questions are required';
+            }
+
+            //validate questions
+            foreach ($json['questions'] as &$question) {
+                //todo: move domains somewhere global
+                if(!array_key_exists('domain', $question)) {
+                    return 'Question is missing the required \'domain\' field';
+                }
+                $domain = $question['domain'];
+                if (!in_array($domain, self::$domains, true)) {
+                    return 'Question contained invalid domain: ' . $domain;
+                }
+
+                // validate all values are strings
+                foreach ($question as $key => $value) {
+                    if (!is_string($value)) {
+                        return 'Expected string value for: '. $key . ' received value: ' . $value;
+                    }
+                    // todo: validate the question field names
+                }
+            }
+
+            unset($question);
+
+
+            // {questions: [/*question*/]}
+            // question (triage): {domain, element, generated_variable, question}
+            // question (survey): {
+            //  domain
+            //  element
+            //  indicator
+            //  variable_suffix
+            //  generated_variable
+            //  dependencies
+            //  phase
+            //  phase_description
+            //  domain_print
+            //  element_print
+            //  indicator_print
+            //  element_description
+            //  question
+            //  question_example
+            //  description
+            //  advice
+            //}
+        } catch (\Exception $e) { /*ignore and fail*/
+            Log::error($e);
+            return 'Exception parsing file: ' . $e->getMessage();
+        }
+        return null;
+    }
+
+}
---
+++ database/migrations/2024_02_06_235812_create_surveys_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('surveys', function (\Illuminate\Database\Schema\Blueprint $table) {
+            $table->increments('version');
+            $table->boolean('is_active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
