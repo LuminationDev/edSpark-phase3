51d94882be319dcd2037a83666a29580a927c47a
Erick Hartawan <ehartawan@lumination.com.au>
2024-02-20 16:12:37 +1030

    initial work on software how to access

--- database/migrations/2024_02_20_045626_add_how_to_access_to_softwares_table.php
+++ database/migrations/2024_02_20_045626_add_how_to_access_to_softwares_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('softwares', function (Blueprint $table) {
-            $table->string('how_to_access')->nullable()->after('extra_content');
+            $table->longText('how_to_access')->nullable()->after('extra_content');
         });
     }
 
