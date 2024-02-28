ca562e88392e55447e3b5e6d2c9e4485889cfb67
Erick Hartawan <ehartawan@lumination.com.au>
2024-02-28 16:15:14 +1030

    removed console log

--- resources/js/components/global/ProfileDropdown.vue
+++ resources/js/components/global/ProfileDropdown.vue
@@ -73,7 +73,6 @@ const avatarUrlWithFallback  = computed(() =>{
     if(imageError.value){
         return avatarUIFallbackURL + currentUser.value.display_name
     } else{
-        console.log(`${imageURL}/${props.avatarUrl}`)
         return `${imageURL}/${props.avatarUrl}`
     }
 })
