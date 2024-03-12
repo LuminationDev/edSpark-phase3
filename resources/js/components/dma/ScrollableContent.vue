579e129bf80acee27b3a529dae9974a930b7fc75
Andrew Carpenter <andrew@codefishstudio.com>
2024-03-13 10:20:27 +1030

    Moved scroll wrapping to ScrpollableContent component, shows a disappearing indicator if section is scrollable

---
+++ resources/js/components/dma/ScrollableContent.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+
+import iconBack from '@/assets/images/dma/icons/arrow-left.svg';
+
+const scrollArea = ref(null);
+
+const showScrollNotice = ref(false);
+
+onMounted(() => {
+    if(scrollArea.value) {
+        if(scrollArea.value.scrollHeight > scrollArea.value.clientHeight) {
+            showScrollNotice.value = true;
+        }
+    }
+});
+
+const handleScroll = () => {
+    if(showScrollNotice.value && scrollArea.value?.scrollTop > 10) {
+        console.log("hide notice");
+        showScrollNotice.value = false;
+    }
+}
+
+</script>
+<template>
+    <div class="flex-1 overflow-hidden scroll-fade">
+        <div
+            ref="scrollArea"
+            class="h-full pb-10 relative z-50 md:overflow-x-none md:overflow-y-scroll"
+            @scroll="handleScroll"
+        >
+            <slot />
+        </div>
+    </div>
+    <div
+        class="absolute bottom-0 align-middle duration-500 opacity-0 scroll-notice transition-opacity"
+        :class="{'opacity-100': showScrollNotice}"
+    >
+        Scroll for more
+        <img
+            :src="iconBack"
+            class="inline rotate-90 scale-x-[-1]"
+        >
+    </div>
+</template>
+
+<style scoped lang="scss">
+@media screen and (min-width: 768px) {
+    .scroll-fade {
+        position: relative;
+        mask-image: linear-gradient(transparent, black 5%, black 90%, transparent);
+    }
+}
+</style>
+
