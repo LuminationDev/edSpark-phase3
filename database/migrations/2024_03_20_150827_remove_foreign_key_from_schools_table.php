da4430c4f175e37290137e29d3080be4a6702360
Erick Hartawan <ehartawan@lumination.com.au>
2024-03-20 15:09:14 +1030

    reverse adding foreign key to school meta

---
+++ database/migrations/2024_03_20_150827_remove_foreign_key_from_schools_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('school_metas', function (Blueprint $table) {
+            $table->dropForeign(['school_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('school_metas', function (Blueprint $table) {
+            // FOREIGN KEY
+            $table->foreign('school_id')
+                ->references('id')->on('schools')->onDelete('cascade');
+        });
+    }
+};
