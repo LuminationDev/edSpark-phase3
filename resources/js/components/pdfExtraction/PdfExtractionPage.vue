3c086b345c563e5cecb84b47b4baa26c5e282391
melwynanthony <manthony@lumination.com.au>
2024-03-26 16:30:43 +1030

    Revert "Revert "test 1 - commit""
    This reverts commit da560fc6eaa7bbd35ce9e2d71ed5cac2c7206b60.

---
+++ resources/js/components/pdfExtraction/PdfExtractionPage.vue
@@ -0,0 +1,25 @@
+
+
+<template>
+    <div class="ml-28 mt-72">
+        <input
+            type="file"
+            accept=".pdf"
+            @change="handleFileUpload"
+        >
+        <div
+            v-if="pdfContent"
+            v-html="pdfContent"
+        />
+        <button @click="convertToJson">
+            Convert to JSON
+        </button>
+        <div v-if="jsonContent">
+            {{ jsonContent }}
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
