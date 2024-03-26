c76d7f6481ec27597dca54d81932e87052099c17
melwynanthony <manthony@lumination.com.au>
2024-03-26 16:29:25 +1030

    Revert "installed pdf reader library and npm pdf-parse"
    This reverts commit 3ef0100e27b92829170193d58771542fa6d30b19.

--- resources/js/components/pdfExtraction/PdfExtractionPage.vue
+++ resources/js/components/pdfExtraction/PdfExtractionPage.vue
@@ -1,41 +1,16 @@
-<script setup>
-import pdfParse from 'pdf-parse';
-import { ref } from 'vue';
 
-const pdfContent = ref(null);
-const jsonContent = ref(null);
-
-const handleFileUpload = async event => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.onload = async () => {
-        const buffer = reader.result;
-        const text = await parsePdf(buffer);
-        pdfContent.value = text;
-    };
-    reader.readAsArrayBuffer(file);
-};
-
-const parsePdf = async (buffer) => {
-    const pdf = await pdfParse(buffer, { max: 0xfffff, pagerender: null, encoding: 'UTF-8' }); // Adjust encoding as needed
-    return pdf.text;
-};
-
-const convertToJson = () => {
-    jsonContent.value = JSON.stringify({ content: pdfContent.value });
-};
-</script>
 
 <template>
-    <div>
+    <div class="ml-28 mt-72">
         <input
             type="file"
             accept=".pdf"
             @change="handleFileUpload"
         >
-        <div v-if="pdfContent">
-            {{ pdfContent }}
-        </div>
+        <div
+            v-if="pdfContent"
+            v-html="pdfContent"
+        />
         <button @click="convertToJson">
             Convert to JSON
         </button>
