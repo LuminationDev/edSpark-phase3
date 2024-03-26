3ef0100e27b92829170193d58771542fa6d30b19
melwynanthony <manthony@lumination.com.au>
2024-03-26 13:47:44 +1030

    installed pdf reader library and npm pdf-parse

--- resources/js/components/pdfExtraction/PdfExtractionPage.vue
+++ resources/js/components/pdfExtraction/PdfExtractionPage.vue
@@ -1,16 +1,41 @@
+<script setup>
+import pdfParse from 'pdf-parse';
+import { ref } from 'vue';
 
+const pdfContent = ref(null);
+const jsonContent = ref(null);
+
+const handleFileUpload = async event => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = async () => {
+        const buffer = reader.result;
+        const text = await parsePdf(buffer);
+        pdfContent.value = text;
+    };
+    reader.readAsArrayBuffer(file);
+};
+
+const parsePdf = async (buffer) => {
+    const pdf = await pdfParse(buffer, { max: 0xfffff, pagerender: null, encoding: 'UTF-8' }); // Adjust encoding as needed
+    return pdf.text;
+};
+
+const convertToJson = () => {
+    jsonContent.value = JSON.stringify({ content: pdfContent.value });
+};
+</script>
 
 <template>
-    <div class="ml-28 mt-72">
+    <div>
         <input
             type="file"
             accept=".pdf"
             @change="handleFileUpload"
         >
-        <div
-            v-if="pdfContent"
-            v-html="pdfContent"
-        />
+        <div v-if="pdfContent">
+            {{ pdfContent }}
+        </div>
         <button @click="convertToJson">
             Convert to JSON
         </button>
