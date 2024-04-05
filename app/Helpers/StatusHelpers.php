6575b3757a8c1b0c9574f17363a19fb00d16ff35
Erick Hartawan <ehartawan@lumination.com.au>
2024-04-05 15:07:39 +1030

    user table fullname nullable and added status helpers

---
+++ app/Helpers/StatusHelpers.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Helpers;
+
+class StatusHelpers
+{
+    const DRAFT = 'DRAFT';
+
+    const PENDING = 'PENDING';
+    const UNPUBLISHED = 'UNPUBLISHED';
+
+    const PUBLISHED = 'PUBLISHED';
+    const ARCHIVED = 'ARCHIVED';
+    const REJECTED = 'REJECTED';
+}
